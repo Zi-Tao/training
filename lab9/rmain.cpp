8512c2dba51f9012d948b2e776fd622bb0a845c4
Zi-Tao <80264433+Zi-Tao@users.noreply.github.com>
2023-05-25 14:39:58 +0800

    lab9

---
+++ lab9/rmain.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ReplaceMyString {
+   public:
+    ReplaceMyString() {}
+
+    void replaceString(string a, string b) {
+        ifstream inputFile(__FILE__);
+        ofstream outputFile("rmain.cpp");
+        if (inputFile.is_open() && outputFile.is_open()) {
+            string line;
+            while (getline(inputFile, line)) {
+                size_t pos = line.find(a);
+                while (pos != string::npos) {
+                    line.replace(pos, a.length(), b);
+                    pos = line.find(a, pos + b.length());
+                }
+                outputFile << line << endl;
+            }
+            inputFile.close();
+            outputFile.close();
+            cout << "成功\n";
+        } else {
+            cout << "無法開啟檔案\n";
+        }
+    }
+};
+
+int main() {
+    ReplaceMyString my;
+    my.replaceString("IU is best", "IU is best is best");
+    return 0;
+}
