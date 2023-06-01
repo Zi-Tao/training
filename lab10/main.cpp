eb4ef0a84d7dff63d07437a92c6246267731a6c9
Zi-Tao <80264433+Zi-Tao@users.noreply.github.com>
2023-06-01 14:34:28 +0800

    lab10

---
+++ lab10/main.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class myString {
+   private:
+    string m_str;
+
+   public:
+    myString(string s) {
+        m_str = s;
+    }
+};
+
+class ReadClass {
+   public:
+    void showClass() {
+        ifstream file("main.cpp");  // 開啟程式碼檔案
+        string line;
+        int i = 0;
+        vector<string> className;
+        int count = 0;
+        // int lastCount = 0; // 儲存最後一次的 count 數值
+        while (getline(file, line)) {                                   // 逐行讀取程式碼
+            size_t classPos = line.find("class ");                      // 尋找 class 關鍵字
+            if (classPos != string::npos) {                             // 如果找到了 class 關鍵字
+                size_t classNameStart = classPos + 6;                   // class 名稱起始位置
+                size_t classNameEnd = line.find(" {", classNameStart);  // class 名稱結束位置
+                if (classNameEnd != string::npos) {
+                    // className = line.substr(classNameStart, classNameEnd - classNameStart); // 提取 class 名稱
+                    className.push_back(line.substr(classNameStart, classNameEnd - classNameStart));
+                    count++;
+                }
+            }
+        }
+        cout << count << " class in main.cpp" << endl;
+        for (int i = 0; i < count; i++) {
+            cout << "class " << className[i] << endl;
+        }
+        file.close();  // 關閉檔案
+    }
+};
+
+int main() {
+    ReadClass rfile;
+    rfile.showClass();
+    return 0;
+}
